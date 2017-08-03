ab6022ad5c902c8f67d4fd492f688a1a00eae8a9
kurageya0307 <y.kurage@gmail.com>
2017-08-03 12:02:47 +0900

    Update KURAGE_Dice.js

--- KURAGE_Dice.js
+++ KURAGE_Dice.js
@@ -15,13 +15,25 @@
  * @plugindesc サイコロプラグイン
  * @author Y.KURAGE
  *
- * @param 結果の代入先の変数
- * @desc サイコロを振った結果を代入する変数番号を指定します。
+ * @param ピクチャ開始番号
+ * @desc このパラメータで指定したピクチャ番号から120個のピクチャを使用します。デフォルトのままならばピクチャ番号 1～120番です。	
  * @default 1
  *
- * @param ピクチャ開始番号
- * @desc このパラメータで指定したピクチャ番号から6つを使用します。デフォルトのままならばピクチャ番号 1～6番が使用されます。	
+ * @param サイコログループ1の結果の代入先
+ * @desc サイコログループ1を振った結果を代入する変数番号を指定します。
  * @default 1
+ *
+ * @param サイコログループ2の結果の代入先
+ * @desc サイコログループ2を振った結果を代入する変数番号を指定します。
+ * @default 2
+ *
+ * @param サイコログループ3の結果の代入先
+ * @desc サイコログループ3を振った結果を代入する変数番号を指定します。
+ * @default 3
+ *
+ * @param サイコログループ4の結果の代入先
+ * @desc サイコログループ4を振った結果を代入する変数番号を指定します。
+ * @default 4
  *
  * @help サイコロをふるプラグインです。
  * プラグインコマンド「showDice」でサイコロを表示します。
@@ -29,11 +41,19 @@
  * サイコロの結果はプラグインパラメータで設定した変数に代入されます。
  *
  * プラグインコマンド
- * 「showDice X Y」
- * 　X Yで指定した座標にサイコロを表示します。
+ * ・「showDice X Y サイコログループ番号 サイコロ個数」
+ * 　　X Yで指定した座標にサイコロを表示します。
+ *　　サイコログループ番号には 1～4の番号を指定します。
+ *　　これらのサイコログループは独立しており，例えば「プレイヤーとエネミーがそれぞれサイコロを振って，値を比較する」ということが可能です。
+ *　　例：showDice 300 600 1 2
  *
- * 「throwDice」
- *　 サイコロを振ります。
+ * ・「throwDice サイコログループ番号」
+ *　　 指定したサイコログループ番号のサイコロを振ります。
+ *　　 サイコログループ番号を省略した場合，全てのサイコログループのサイコロを振ります。
+ *
+ * ・「removeDice サイコログループ番号」
+ *　　 指定したサイコログループ番号のサイコロを削除します。
+ *　　 サイコログループ番号を省略した場合，全てのサイコログループのサイコロを削除します。
  *
  * ライセンス：
  *  本プラグインはMITライセンスです。
@@ -48,9 +68,20 @@
     // パラメータの取得
     //=============================================================================
     var parameters = PluginManager.parameters('KURAGE_Dice');
-    var variables_index = Number(parameters['結果の代入先の変数'] || 0);
-    var picture_id_start = Number(parameters['ピクチャ開始番号'] || 0);
+    var var_index_1 = Number(parameters['サイコログループ1の結果の代入先'] || 1);
+    var var_index_2 = Number(parameters['サイコログループ1の結果の代入先'] || 2);
+    var var_index_3 = Number(parameters['サイコログループ1の結果の代入先'] || 3);
+    var var_index_4 = Number(parameters['サイコログループ1の結果の代入先'] || 4);
+    var var_indices = [var_index_1, var_index_2, var_index_3, var_index_4];
+    var picture_id_start = Number(parameters['ピクチャ開始番号'] || 1);
 
+    //=============================================================================
+    // パラメータの取得
+    //=============================================================================
+    const MAX_DICE_GROUP = 4;
+    const MAX_DICE_NUM   = 5;
+    const OFFSET_X = [0, 64, 32, 96, 64];
+    const OFFSET_Y = [0, 0, 64, 64, 128];
     //=============================================================================
     //  プラグインコマンドを追加定義
     //=============================================================================
@@ -60,12 +91,72 @@
         _Game_Interpreter_pluginCommand.call(this, command, args);
         switch(command) {
             case 'showDice':
-                var x = parseInt(args[0], 10).clamp(1, 400) || 1;
-                var y = parseInt(args[1], 10).clamp(1, 400) || 1;
-                $gameScreen.showDice(picture_id_start, 0, x, y, 100.0, 100.0, 255, 0);
+                var x          = parseInt(args[0], 10) || 400;
+                var y          = parseInt(args[1], 10) || 400;
+                var dice_group = parseInt(args[2], 10) || 1;
+                dice_group--;
+                if(dice_group >= MAX_DICE_GROUP || dice_group < 0 )
+                {
+                    console.log(dice_group);
+                    throw new Error;
+                }
+                var dice_num   = parseInt(args[3], 10).clamp(1, 400) || 1;
+                if(dice_num > MAX_DICE_NUM || dice_num < 0 )
+                {
+                    console.log(dice_num);
+                    throw new Error;
+                }
+                $gameScreen.showDice(x, y, dice_group, dice_num);
                 break;
             case 'throwDice':
-                $gameScreen.throwDice(100, 100);
+                var dice_groups = [parseInt(args[0], 10),
+                                   parseInt(args[1], 10),
+                                   parseInt(args[2], 10),
+                                   parseInt(args[3], 10)];
+                if( !dice_groups[0] )
+                {
+                    for(var k=0; k<MAX_DICE_GROUP; k++) {
+                        dice_group = k;
+                        $gameScreen.throwDice(dice_group);
+                    }
+                } else {
+                    for(var k=0; k<MAX_DICE_GROUP; k++) {
+                        if( !dice_groups[k] )
+                            break;
+                        dice_group = dice_groups[k] - 1;
+                        if(dice_group >= MAX_DICE_GROUP || dice_group < 0 )
+                        {
+                            console.log(dice_group);
+                            throw new Error;
+                        }
+                        $gameScreen.throwDice(dice_group);
+                    }
+                }
+                break;
+            case 'removeDice':
+                var dice_groups = [parseInt(args[0], 10),
+                                   parseInt(args[1], 10),
+                                   parseInt(args[2], 10),
+                                   parseInt(args[3], 10)];
+                if( !dice_groups[0] )
+                {
+                    for(var k=0; k<MAX_DICE_GROUP; k++) {
+                        dice_group = k;
+                        $gameScreen.removeDice(dice_group);
+                    }
+                } else {
+                    for(var k=0; k<MAX_DICE_GROUP; k++) {
+                        if( !dice_groups[k] )
+                            break;
+                        dice_group = dice_groups[k] - 1;
+                        if(dice_group >= MAX_DICE_GROUP || dice_group < 0 )
+                        {
+                            console.log(dice_group);
+                            throw new Error;
+                        }
+                        $gameScreen.removeDice(dice_group);
+                    }
+                }
                 break;
         }
     };
@@ -118,18 +209,34 @@
         }
     };
     
-    Game_Screen.prototype.showDice = function(pictureId, origin, x, y,
-                                                 scaleX, scaleY, opacity, blendMode) {
-        this._dice_pictures = [];
-        for(var i=0; i<6; i++) {
-            var realPictureId = this.realPictureId(pictureId + i);
-            var name = "dice_" + String(i+1);
-            this._dice_pictures[i] = new Dice_Picture();
-            this._dice_pictures[i].show(name, 0, x, y, 100, 100, 0, 0);
-            this._pictures[realPictureId] = this._dice_pictures[i];
+    var _Game_Screen_initialize = Game_Screen.prototype.initialize;
+    Game_Screen.prototype.initialize = function() {
+        _Game_Screen_initialize.call(this);
+        this._dice_picture_3d_array = [];
+        this._dice_indices = [];
+        this._dice_throwings = [];
+        for(var i=0; i<MAX_DICE_GROUP; i++) {
+            this._dice_picture_3d_array[i] = [];
+            this._dice_indices[i] = [];
+            this._dice_throwings[i] = [];
         }
-        this._dice_index = 0;
-        this._dice_throwing = false;
+    };
+
+    Game_Screen.prototype.showDice = function(x, y, dice_group, dice_num) {
+        for(var j=0; j<MAX_DICE_NUM; j++) {
+            this._dice_picture_3d_array[dice_group][j] = [];
+        }
+        for(var j=0; j<dice_num; j++) {
+            for(var i=0; i<6; i++) {
+                var realPictureId = this.realPictureId(picture_id_start + i + j*6 + dice_group*6*MAX_DICE_NUM);
+                var name = "dice_" + String(i+1);
+                this._dice_picture_3d_array[dice_group][j][i] = new Dice_Picture();
+                this._dice_picture_3d_array[dice_group][j][i].show(name, 0, x + OFFSET_X[j], y + OFFSET_Y[j], 100, 100, 255, 0);
+                this._pictures[realPictureId] = this._dice_picture_3d_array[dice_group][j][i];
+            }
+            this._dice_indices[dice_group][j]   = 0;
+            this._dice_throwings[dice_group][j] = false;
+        }
     };
     
     Array.prototype.shuffle = function() {
@@ -137,69 +244,97 @@
                      .sort(function(a, b){return a[1] - b[1]})
                      .map(function(a){return a[0]});
     }
-    Game_Screen.prototype.throwDice = function(target_x, target_y) {
-        this._dice_throwing = true;
-        var shuffle_val = [1, 2, 3, 4, 5, 6].shuffle();
-        var final_value    = shuffle_val[0]
-        $gameVariables.setValue(variables_index, final_value);
-        var throwing_val_1 = shuffle_val[1];
-        var throwing_val_2 = shuffle_val[2];
-        var throwing_val_3 = shuffle_val[3];
-        for(var i=0; i<6; i++) {
-            var tmp = [];
-            var x = this._dice_pictures[this._dice_index].x();
-            var y = this._dice_pictures[this._dice_index].y();
-            if( (i+1)==throwing_val_1 ) { 
-                tmp.push({_duration:1, _target_x:x, _target_y:y, _target_opacity:255});
-                tmp.push({_duration:6, _target_x:x-20, _target_y:y-150, _target_opacity:255});
-                tmp.push({_duration:8, _target_x:x-40, _target_y:y-180, _target_opacity:255});
-            } else {
-                tmp.push({_duration:1, _target_x:x, _target_y:y, _target_opacity:0});
-                tmp.push({_duration:6, _target_x:x-20, _target_y:y-150, _target_opacity:0});
-                tmp.push({_duration:8, _target_x:x-40, _target_y:y-180, _target_opacity:0});
+    Game_Screen.prototype.throwDice = function(dice_group) {
+        var final_value    = 0;
+        if ( this._dice_picture_3d_array[dice_group].length > 0 ) {
+            for(var j=0; j<MAX_DICE_NUM; j++) {
+                if ( this._dice_picture_3d_array[dice_group][j].length <= 0 )
+                    break;
+                var shuffle_val = [1, 2, 3, 4, 5, 6].shuffle();
+                var throwing_val_1 = shuffle_val[1];
+                var throwing_val_2 = shuffle_val[2];
+                var throwing_val_3 = shuffle_val[3];
+                var throwing_val_4 = shuffle_val[0];
+                this._dice_throwings[dice_group][j] = true;
+                final_value += throwing_val_4;
+                for(var i=0; i<6; i++) {
+                    var tmp = [];
+                    var x = this._dice_picture_3d_array[dice_group][j][i].x();
+                    var y = this._dice_picture_3d_array[dice_group][j][i].y();
+                    if( (i+1)==throwing_val_1 ) { 
+                        tmp.push({_duration:1, _target_x:x, _target_y:y, _target_opacity:255});
+                        tmp.push({_duration:6, _target_x:x-20, _target_y:y-150, _target_opacity:255});
+                        tmp.push({_duration:8, _target_x:x-40, _target_y:y-180, _target_opacity:255});
+                    } else {
+                        tmp.push({_duration:1, _target_x:x, _target_y:y, _target_opacity:0});
+                        tmp.push({_duration:6, _target_x:x-20, _target_y:y-150, _target_opacity:0});
+                        tmp.push({_duration:8, _target_x:x-40, _target_y:y-180, _target_opacity:0});
+                    }
+                    if( (i+1)==throwing_val_2 ) { 
+                        tmp.push({_duration:1, _target_x:x-40, _target_y:y-180, _target_opacity:255});
+                        tmp.push({_duration:10, _target_x:x-60, _target_y:y-150, _target_opacity:255});
+                        tmp.push({_duration:10, _target_x:x-80, _target_y:y-80, _target_opacity:255});
+                    } else {
+                        tmp.push({_duration:1, _target_x:x-40, _target_y:y-180, _target_opacity:0});
+                        tmp.push({_duration:10, _target_x:x-60, _target_y:y-150, _target_opacity:0});
+                        tmp.push({_duration:10, _target_x:x-80, _target_y:y-80, _target_opacity:0});
+                    }
+                    if( (i+1)==throwing_val_3 ) { 
+                        tmp.push({_duration:1, _target_x:x-80, _target_y:y-80, _target_opacity:255});
+                        tmp.push({_duration:8, _target_x:x-100, _target_y:y-140, _target_opacity:255});
+                        tmp.push({_duration:8, _target_x:x-110, _target_y:y-160, _target_opacity:255});
+                    } else {
+                        tmp.push({_duration:1, _target_x:x-80, _target_y:y-80, _target_opacity:0});
+                        tmp.push({_duration:8, _target_x:x-100, _target_y:y-140, _target_opacity:0});
+                        tmp.push({_duration:8, _target_x:x-110, _target_y:y-160, _target_opacity:0});
+                    }
+                    if( (i+1)==throwing_val_4 ) { 
+                        tmp.push({_duration:1, _target_x:x-110, _target_y:y-160, _target_opacity:255});
+                        tmp.push({_duration:8, _target_x:x-120, _target_y:y-140, _target_opacity:255});
+                    } else {
+                        tmp.push({_duration:1, _target_x:x-110, _target_y:y-160, _target_opacity:0});
+                        tmp.push({_duration:8, _target_x:x-120, _target_y:y-140, _target_opacity:0});
+                    }
+        
+                    this._dice_picture_3d_array[dice_group][j][i].setMoves(tmp);
+                }
             }
-            if( (i+1)==throwing_val_2 ) { 
-                tmp.push({_duration:1, _target_x:x-40, _target_y:y-180, _target_opacity:255});
-                tmp.push({_duration:10, _target_x:x-60, _target_y:y-150, _target_opacity:255});
-                tmp.push({_duration:10, _target_x:x-80, _target_y:y-80, _target_opacity:255});
-            } else {
-                tmp.push({_duration:1, _target_x:x-40, _target_y:y-180, _target_opacity:0});
-                tmp.push({_duration:10, _target_x:x-60, _target_y:y-150, _target_opacity:0});
-                tmp.push({_duration:10, _target_x:x-80, _target_y:y-80, _target_opacity:0});
-            }
-            if( (i+1)==throwing_val_3 ) { 
-                tmp.push({_duration:1, _target_x:x-80, _target_y:y-80, _target_opacity:255});
-                tmp.push({_duration:8, _target_x:x-100, _target_y:y-140, _target_opacity:255});
-                tmp.push({_duration:8, _target_x:x-110, _target_y:y-160, _target_opacity:255});
-            } else {
-                tmp.push({_duration:1, _target_x:x-80, _target_y:y-80, _target_opacity:0});
-                tmp.push({_duration:8, _target_x:x-100, _target_y:y-140, _target_opacity:0});
-                tmp.push({_duration:8, _target_x:x-110, _target_y:y-160, _target_opacity:0});
-            }
-            if( (i+1)==final_value ) { 
-                tmp.push({_duration:1, _target_x:x-110, _target_y:y-160, _target_opacity:255});
-                tmp.push({_duration:8, _target_x:x-120, _target_y:y-140, _target_opacity:255});
-            } else {
-                tmp.push({_duration:1, _target_x:x-110, _target_y:y-160, _target_opacity:0});
-                tmp.push({_duration:8, _target_x:x-120, _target_y:y-140, _target_opacity:0});
-            }
+        }
+        $gameVariables.setValue(var_indices[dice_group], final_value);
+    };
     
-            this._dice_pictures[i].setMoves(tmp);
+    Game_Screen.prototype.removeDice = function(dice_group) {
+        if ( this._dice_picture_3d_array[dice_group].length > 0 ) {
+            for(var j=0; j<MAX_DICE_NUM; j++) {
+                if ( this._dice_picture_3d_array[dice_group][j].length <= 0 )
+                    break;
+                for(var i=0; i<6; i++) {
+                    this.erasePicture(picture_id_start + i + j*6 + dice_group*6*MAX_DICE_NUM);
+                }
+            }
         }
     };
     
     var _Game_Screen_updatePictures = Game_Screen.prototype.updatePictures;
     Game_Screen.prototype.updatePictures = function() {
-        if( this._dice_pictures && !this._dice_throwing) {
-            var x = this._dice_pictures[this._dice_index].x();
-            var y = this._dice_pictures[this._dice_index].y();
-            this._dice_pictures[this._dice_index].move(0, x, y, 100, 100, 0, 0, 1);
-            this._dice_index++;
-            if(this._dice_index >= 6){
-                this._dice_index = 0;
+        for(var k=0; k<MAX_DICE_GROUP; k++) {
+            for(var j=0; j<MAX_DICE_NUM; j++) {
+                if( this._dice_picture_3d_array[k][j] && this._dice_picture_3d_array[k][j].length  > 0 && !this._dice_throwings[k][j]) {
+                    var i = this._dice_indices[k][j];
+                    var x = this._dice_picture_3d_array[k][j][i].x();
+                    var y = this._dice_picture_3d_array[k][j][i].y();
+                    this._dice_picture_3d_array[k][j][i].move(0, x, y, 100, 100, 0, 0, 1);
+                    this._dice_indices[k][j] += 1;
+                    if(this._dice_indices[k][j] >= 6){
+                        this._dice_indices[k][j] = 0;
+                    }
+                    i = this._dice_indices[k][j];
+                    this._dice_picture_3d_array[k][j][i].move(0, x, y, 100, 100, 255, 0, 1);
+                }
             }
-            this._dice_pictures[this._dice_index].move(0, x, y, 100, 100, 255, 0, 1);
         }
+      /*
+        */
         _Game_Screen_updatePictures.call(this);
       
     };
